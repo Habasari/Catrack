ad50dd8a518d209abea9d68a4aefaea02e6b137b
pasketi <pasketi@gmail.com>
2016-05-10 12:56:57 +0300

    asdfgh

--- Catrack/index.php
+++ Catrack/index.php
@@ -7,17 +7,20 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>C(at)Trak</title>
     </head>
     <body>
+        <div style = logo>
+        </div>
         <?php
         require_once('config.php');
         require_once('dbopen.php');
-        
+
         $query = "SELECT * FROM koord";
         $results = mysql_query($query)
                 or die("Kyselyssä tapahtui virhe: " . mysql_errno());
-        
+
         while($row = mysql_fetch_array($results)) {
             echo "<br>";
             echo "$row[0]";
