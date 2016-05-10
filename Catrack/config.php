1d793e4936025deadc099a71f5421900278f1516
toli <toli@anatolis-mbp.home>
2016-05-10 23:43:21 +0300

    tabit jne formatointi

--- Catrack/config.php
+++ Catrack/config.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,5 +11,4 @@ $dbpass = 'MySQLpAssUtuSSu13';
 $dbhost = 'mysql.metropolia.fi';
 $dbname = 'anatolil';
 $dbtype = 'mysql';
-
 ?>
